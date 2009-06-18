f4741417e76d61ee371380b562246bd7b263ade7
Matthew Ranostay <mranostay@embeddedalley.com>
2009-06-18 15:47:20 -0400

    Add Android makefiles for Lirc.
    Signed-off-by: Matthew Ranostay <mranostay@embeddedalley.com>

--- Android.mk
+++ Android.mk
@@ -9,3 +9,6 @@ include $(CLEAR_VARS)
 include $(LIRC_TOP)/daemons/libhw_module.mk
 include $(LIRC_TOP)/daemons/lircd.mk
 include $(LIRC_TOP)/tools/irw.mk
+include $(LIRC_TOP)/tools/liblirc_client.mk
+include $(LIRC_TOP)/tools/irexec.mk
+include $(LIRC_TOP)/tools/lircsimkey.mk
